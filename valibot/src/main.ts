60fce99a13dcb5e48f59bc6c98a0990ba6d1a1b7
Gaku Yasui <gaku.yasui@gmail.com>
2024-10-06 00:50:21 +0900

    Add valibot

---
+++ valibot/src/main.ts
@@ -0,0 +1,24 @@
+import * as v from 'valibot';
+
+const LoginSchema = v.object({
+  email: v.pipe(
+    v.string('Your email must be a string.'),
+    v.nonEmpty('Please enter your email.'),
+    v.email('The email address is badly formatted.')
+  ),
+  password: v.pipe(
+    v.string('Your password must be a string.'),
+    v.nonEmpty('Please enter your password.'),
+    v.minLength(8, 'Your password must have 8 characters or more.')
+  ),
+});
+
+
+type LoginData = v.InferOutput<typeof LoginSchema>;
+
+function getLoginData(data: unknown): LoginData {
+  return v.parse(LoginSchema, data);
+}
+
+console.log(getLoginData({ email: "test@example.com", password: "test-password" }))
+console.log(getLoginData({ email: "test-example.com", password: "test" })) // ValiError
